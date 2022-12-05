0058bcb673b6ddda8a32ff0a3b0e408a85c37e79
Emir Ben <114371873+EMIRBEN001@users.noreply.github.com>
2022-12-05 21:17:47 +0700

    Add files via upload

---
+++ hci-forum-week-9/decipher.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-undef */
+/* eslint-disable one-var */
+function digitalDecipher (eMssg, key) {
+  const alphabets = ['buffer', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+  const keyString = key.toString()
+  const output = [], word = []
+
+  for (let i = 0; i < eMssg.length; i++) {
+    if (j === keyString.length) {
+      j = 0
+    }
+    output[i] = eMssg[i] - keyString[j]
+    word[i] = alphabets[output[i]]
+    j++
+  }
+
+  // eslint-disable-next-line quotes
+  const txt = word.join("")
+  console.log(txt)
+}
+
+digitalDecipher([20, 12, 18, 30, 21], 1939)
+digitalDecipher([14, 30, 11, 1, 20, 17, 18, 18], 1990)
+digitalDecipher([6, 4, 1, 3, 9, 20], 100)
